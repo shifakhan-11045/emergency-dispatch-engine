4553075426b2fc043a2795de8a10dac53dafd5b0
Shifa <shifakhan11045@gmail.com>
2026-02-05 00:29:30 +0530

    Add city data providers and remove CityBuilder

--- src/App.java
+++ src/App.java
@@ -1,15 +1,17 @@
-import com.emergency.engine.CityBuilder;
+import com.emergency.engine.CityDataProvider;
 import com.emergency.engine.CityGraph;
+import com.emergency.engine.ConsoleCityDataProvider;
 
 public class App {
 
     public static void main(String[] args) {
 
-        CityBuilder cityBuilder = new CityBuilder();
-        CityGraph graph = cityBuilder.buildCity();
+        CityDataProvider provider = new ConsoleCityDataProvider();
+                // or new SampleCityDataProvider();
 
+        CityGraph graph = provider.buildCity();
         graph.printGraph();
 
-        System.out.println("City loaded dynamically");
+        System.out.println("City loaded successfully");
     }
 }
---
+++ src/com/emergency/engine/CityDataProvider.java
@@ -0,0 +1,5 @@
+package com.emergency.engine;
+
+public interface CityDataProvider {
+    CityGraph buildCity();
+}
